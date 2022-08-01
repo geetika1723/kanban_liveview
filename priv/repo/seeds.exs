fd43fdd6f605b5d5790ece76ff90cd4c296dab1b
geetika1723 <geetikasethi1723@gmail.com>
2022-08-01 18:05:59 +0530

    Backend updated

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+board = KanbanLiveview.Repo.insert!(%KanbanLiveview.Board{title: "Awesome project"})
+
+backlog = KanbanLiveview.Repo.insert!(%KanbanLiveview.Column{title: "Backlog", board_id: board.id})
+
+_in_progress = KanbanLiveview.Repo.insert!(%KanbanLiveview.Column{title: "In progress", board_id: board.id})
+_done = KanbanLiveview.Repo.insert!(%KanbanLiveview.Column{title: "Done", board_id: board.id})
+
+_card = KanbanLiveview.Repo.insert!(%KanbanLiveview.Card{content: "Put some nice cat picture on the homepage", column_id: backlog.id})
